7dc212755c54c26cd76c1d98533dac02f2f51d54
vartika <vartikav2005@gmail.com>
2026-07-20 20:14:57 +0530

    Add backend authentication setup

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,13 @@
-// Express app setup placeholder
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+module.exports = app;
